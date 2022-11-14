8c5fcf50224c1c5e07b97c05d603b3c39dee4f23
Ahmed Hussein <wolktheline@yahoo.com>
2022-11-14 08:49:25 -0700

    data attributes

--- script.js
+++ script.js
@@ -98,3 +98,6 @@ console.log(logo.getAttribute('src'));
 const link = document.querySelector('.nav__link--btn');
 console.log(link.getAttribute('href')); // this will get relative path
 console.log(link.href); // this will bring absolute path
+
+//Data attributes
+console.log(logo.dataset.versionNumber);
