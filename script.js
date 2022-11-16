c6790d6d0d901ddaedc5311d80e720d53cce7a44
Ahmed Hussein <wolktheline@yahoo.com>
2022-11-15 17:26:11 -0700

    scrollIntoView() method for modern browsers

--- script.js
+++ script.js
@@ -130,9 +130,11 @@ btnScrollTo.addEventListener('click', function (e) {
   //   s1coords.top + window.pageYOffset
   // );
 
-  window.scrollTo({
-    left: s1coords.left + window.pageXOffset,
-    top: s1coords.top + window.pageYOffset,
-    behavior: 'smooth',
-  });
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
 });
