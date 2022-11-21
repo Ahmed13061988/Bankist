031dd15e7d7b5990033ea5117607526f5cfbdf88
Ahmed Hussein <wolktheline@yahoo.com>
2022-11-20 19:05:32 -0700

    creating a random color

--- script.js
+++ script.js
@@ -164,3 +164,8 @@ setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 //rgb(255, 255, 255);
 const randomInt = (min, max) =>
   Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+
+console.log(randomColor(0, 255));
