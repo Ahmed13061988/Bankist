c6799616c90f8fac11b31bf2808eef4e68e75919
Ahmed Hussein <wolktheline@yahoo.com>
2022-11-29 21:18:20 -0700

    getAttribute() method

--- script.js
+++ script.js
@@ -74,6 +74,7 @@ document.querySelectorAll('.nav__link').forEach(function (el) {
   el.addEventListener('click', function (e) {
     e.preventDefault();
     const id = this.getAttribute('href');
+    console.log(id);
   });
 }); // return nodelist
 
