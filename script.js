2337058bfaa5d6953481ae9648575cf454b93a05
Ahmed Hussein <wolktheline@yahoo.com>
2022-11-30 19:43:16 -0700

    using event doligation

--- script.js
+++ script.js
@@ -70,14 +70,21 @@ btnScrollTo.addEventListener('click', function (e) {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
 
-document.querySelectorAll('.nav__link').forEach(function (el) {
-  el.addEventListener('click', function (e) {
-    e.preventDefault();
-    const id = this.getAttribute('href');
-    console.log(id);
-    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
-  });
-}); // return nodelist
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+//event doligation
+
+// 1. Add eventListenner to a common parent element
+// 2. determine what element originated the event
+navLinks.addEventListener('click', function (e) {
+  console.log(e.target);
+});
 
 /////////
 // console.log(document.documentElement);
